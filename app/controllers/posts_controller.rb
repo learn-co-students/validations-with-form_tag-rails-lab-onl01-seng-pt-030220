33d0f1e610594e5e953b2d96921307a62d5c87d1
iamcid <iamcid17@gmail.com>
2020-07-18 00:39:57 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-
+  
   def edit
     @post = Post.find(params[:id])
   end
@@ -10,9 +10,11 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
